eca568a6d9f771a6a90ce44ed859e7a04ded708b
thanglhgcd201414 <95120898+thanglhgcd201414@users.noreply.github.com>
2023-02-02 13:35:00 +0700

    UP

---
+++ 300123_Classwork1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _300123_Classwork1
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Person p = new Person();
+      p.Name = "LE VAN A";
+      p.Age = 20;
+      System.Console.WriteLine(p);
+    }
+  }
+}
